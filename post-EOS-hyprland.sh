085f1a20db0e5ff3ce754e99c375902eca6c869f
magyx <w.magyx@gmail.com>
2024-08-25 18:25:19 +0200

    Add hyprland configs

--- post-EOS-hyprland.sh
+++ post-EOS-hyprland.sh
@@ -3,3 +3,14 @@ yay -Syyu
 yay -S timeshift
 timeshift --btrfs
 sudo timeshift --create --comments 'Minimal arch'
+
+yay -S --needed \
+ hyprland-git \
+ xdg-desktop-portal-hyprland
+
+yay -S --needed \
+ alacritty \
+ nautilus \
+ gvfs-smb \
+ playerctl \
+ btop
