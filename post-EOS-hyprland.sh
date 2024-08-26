d1a559de1d36af0e6fde8172a3bbb2b71d1d2d63
magyx <w.magyx@gmail.com>
2024-08-26 19:05:07 +0200

    add gnome settings and polkit

--- post-EOS-hyprland.sh
+++ post-EOS-hyprland.sh
@@ -5,12 +5,33 @@ timeshift --btrfs
 sudo timeshift --create --comments 'Minimal arch'
 
 yay -S --needed \
- hyprland-git \
- xdg-desktop-portal-hyprland
+  speech-dispatcher
+
+# Hyprland Apps
+yay -S --needed \
+  alacritty \
+  nautilus \
+  gvfs-smb \
+  playerctl \
+  btop \
+  gnome-control-center
+
+# Theme
+yay -S --needed \
+  gnome-themes-extra \
+  adwaita-qt5-git \
+  adwaita-qt6-git \
+  bibata-cursor-theme-bin
 
 yay -S --needed \
- alacritty \
- nautilus \
- gvfs-smb \
- playerctl \
- btop
+  ttf-hack-nerd
+
+# Hyprland
+yay -S --needed \
+  hyprland-git \
+  xdg-desktop-portal-hyprland \
+  qt6-wayland \
+  qt5-wayland \
+  xorg-xhost \
+  polkit-kde-agent
+
